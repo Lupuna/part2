172de27fc274396c406e32c542619f4a4d0854cf
Lupuna <90504544+Lupuna@users.noreply.github.com>
2022-04-14 15:28:01 +0700

    Add files via upload

---
+++ day10/part2.py
@@ -0,0 +1,26 @@
+with open('input.txt') as home0:
+    home = home0.read()
+
+cycles = 0
+
+while cycles <= 49:
+    item = home[0]
+    number = 1
+    end = ''
+    for i in (home+'x')[1:]:
+        if item == i:
+            number +=1
+        else:
+            end += str(number) + item
+            item = i
+            number = 1
+
+    home = end
+
+    cycles +=1
+
+output = open('output2.txt', 'w')
+output.write(str(len(end)))
+
+output.close()
+home0.close()
