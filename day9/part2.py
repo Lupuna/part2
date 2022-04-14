dab02a7e5c2f0d974d2673aab8d35ccc1a620ead
Lupuna <90504544+Lupuna@users.noreply.github.com>
2022-04-14 14:49:00 +0700

    Add files via upload

---
+++ day9/part2.py
@@ -0,0 +1,30 @@
+import itertools
+
+with open('input.txt') as home0:
+    home = home0.read().split('\n')
+
+
+dist = dict()
+loc = set()
+for i in home:
+    start,_,end,_,qp = i.split(' ')
+    dist[(start,end)] = int(qp)
+    dist[(end,start)] = int(qp)
+    loc.add(start)
+    loc.add(end)
+
+best = 0
+for raw in itertools.permutations(loc):
+    km = 0
+    for start, end in zip(raw, raw[1:]):
+
+        km += dist[(start,end)]
+
+    if km > best:
+        best = km
+print(best)
+# output = open('output2.txt', 'w')
+# output.write(str(best))
+#
+# output.close()
+# home0.close()
